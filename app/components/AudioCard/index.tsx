e60b439c9f4cc7fb36c9b69b7606790416ca58cf
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2024-03-28 22:24:31 -0700

    add functionality

---
+++ app/components/AudioCard/index.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Howl } from "howler"
+import { useEffect, useRef, useState } from "react"
+import styles from './index.module.css'
+import { Pause, Play, StopCircle } from "react-feather"
+
+
+export default function AudioCard(props: any) {
+    const runId = useRef<NodeJS.Timeout>()
+    const [timestamp, setTimestamp] = useState(0)
+    const [audio, setAudio] = useState<Howl>()
+
+    
+    useEffect(() => {
+        setAudio(new Howl({
+            src: ['audio/' + props.hit.name],
+            html5: true,
+            onplay: () => {
+                if(runId.current) return;
+                runId.current = setInterval(() => {
+                    setTimestamp(timestamp => timestamp + 1)
+                }, 100)
+            },
+            onpause: () => {
+                clearInterval(runId.current)
+                runId.current = undefined
+            },
+            onend: () => {
+                clearInterval(runId.current)
+                runId.current = undefined
+                setTimestamp(0)
+            },
+            onstop: () => {
+                clearInterval(runId.current)
+                runId.current = undefined
+                setTimestamp(0)
+            }
+        }))
+    }, [])
+    
+    if(!props.hit.name || !audio) return
+
+    return (
+        <div className={styles.card}>
+            <p className={styles.title}>{props.hit.name.substring(props.hit.name.lastIndexOf('/') + 1)}</p>
+            <button className={styles.button} onClick={() => {if(runId.current) return; audio.play()}}><Play /></button>
+            <button className={styles.button} onClick={() => {audio.pause()}}><Pause /></button>
+            <button className={styles.button} onClick={() => {audio.stop()}}><StopCircle /></button>
+            <progress className={styles.progress} max={audio.duration() * 10} value={timestamp} />
+        </div>
+    )
+}
---
+++ app/components/AudioList/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import algoliasearch from "algoliasearch";
+import { Hits, InstantSearch, SearchBox } from "react-instantsearch";
+import AudioCard from "../AudioCard";
+
+interface AudioListProps {
+    // files: string[]
+}
+
+export default function AudioList(props: AudioListProps) {
+    const client = algoliasearch('GR1ZJVDLAO', '5ab70842b6168ad0eda2d72260339ed4')
+
+  return (
+    <div>
+      <InstantSearch searchClient={client} indexName="audio">
+        <SearchBox />
+        <Hits hitComponent={AudioCard} />
+      </InstantSearch>
+    </div>
+  );
+}
