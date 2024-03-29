168f79a574bd2d1c3bbff02b2ff62845c18c5dc6
Ben Meier <38326866+BenMeie@users.noreply.github.com>
2024-03-29 15:47:56 -0700

    readme, home tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-
-import { readdirSync, writeFileSync } from "fs";
 import AudioList from "./components/AudioList";
 
-export default function Home() {
 
-  // const files = readdirSync('./public/audio')
-  // writeFileSync('files_list.json', JSON.stringify(files.map(file => {return {name: file}})))
+export default async function Home() {
 
   return <AudioList />
 }
